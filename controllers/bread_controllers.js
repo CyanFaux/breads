7e9e0c2bacdfd5552f248b5b8a95c144099a5ec5
CyanFaux <santafox26@gmail.com>
2024-01-21 11:04:25 -0500

    "everything pre-crash at Bread READ Route: Index"

---
+++ controllers/bread_controllers.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const breads = express.Router()
+const Bread = require('../models/bread.js')
+// INDEX
+breads.get('/', (req, res) => {
+  res.send('This is the index at /breads')
+})
+
+module.exports = breads
